a2b16018160b52fc72cb3402459cb7106e6b000d
Feroz Ally <feroz.ally@derivco.co.za>
2016-10-25 16:21:02 +0200

    test

--- Test Console App/Program.cs
+++ Test Console App/Program.cs
@@ -13,6 +13,7 @@ namespace Test_Console_App
             Console.WriteLine("This is a new console app");
             Console.WriteLine("This is a new console app #2");
             Console.WriteLine("This is a new console app #3");
+            Console.WriteLine("This is a new console app #3");
             Console.ReadLine();
         }
     }
